4fa4b6001eca6420bbeae4e159929a4203ca1192
anthony <anthonydev03@gmail.com>
2023-07-03 12:11:11 +0200

    norminette 20%

--- srcs/main.c
+++ srcs/main.c
@@ -38,42 +38,56 @@ void    stack_a_init(int *data, Stack *stack_a, int argc)
         i--;
     }
 }
-void pushTwoSmallest(Stack *stack_a, Stack *stack_b) {
+void performPushes(Stack *stack_a, Stack *stack_b, int smallest1, int smallest2)
+{
+    int count = 0;
+    Node *current = stack_a->head;
 
-    int smallest1; 
-    int smallest2;
-    int data;
-    int count;
-    Node *nextNode;
-    Node *current; 
-
-    smallest1 = INT_MAX; 
-    smallest2 = INT_MAX; 
-    current = stack_a->head;
-    while (current != NULL) {
-        data = current->data;
-        if (data < smallest1) {
-            smallest2 = smallest1;
-            smallest1 = data;
-        } else if (data < smallest2 && data != smallest1) {
-            smallest2 = data;
-        }
-        current = current->next;
-    }
-
-    count = 0;
-    current = stack_a->head;
-    while (current != NULL && count < 2) {
-        nextNode = current->next;
-        if (current->data == smallest1 || current->data == smallest2) {
+    while (current != NULL && count < 2)
+    {
+        Node *nextNode = current->next;
+        if (current->data == smallest1 || current->data == smallest2)
+        {
             pb(stack_a, stack_b);
             count++;
-        } else {
+        }
+        else
+        {
             ra(stack_a);
         }
         current = nextNode;
     }
 }
+void findTwoSmallest(Stack *stack_a, int *smallest1, int *smallest2)
+{
+    *smallest1 = INT_MAX;
+    *smallest2 = INT_MAX;
+    Node *current = stack_a->head;
+
+    while (current != NULL)
+    {
+        int data = current->data;
+        if (data < *smallest1)
+        {
+            *smallest2 = *smallest1;
+            *smallest1 = data;
+        }
+        else if (data < *smallest2 && data != *smallest1)
+        {
+            *smallest2 = data;
+        }
+        current = current->next;
+    }
+}
+void pushTwoSmallest(Stack *stack_a, Stack *stack_b)
+{
+    int smallest1;
+    int smallest2;
+
+    findTwoSmallest(stack_a, &smallest1, &smallest2);
+    performPushes(stack_a, stack_b, smallest1, smallest2);
+}
+
 void convertDecimalToBinary(int *decimalArray, int *binaryArray, int size)
 {
     int i = 0;
@@ -236,11 +250,11 @@ int main(int argc, char **argv) {
     if (issorted(stack_a) == true) {
         exit(EXIT_SUCCESS);
     }
-    print_stack(stack_a);
+//    print_stack(stack_a);
     if (argc == 4)
         sort_3_elements(stack_a);
     if (argc == 6)
         sort_5_elements(stack_a, stack_b);
- print_stack(stack_a);
+ //print_stack(stack_a);
      return (0);
 }
