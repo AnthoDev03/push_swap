29e51acd79c9e57c98fe8aea9e9bed541ce770ce
anthony <anthonydev03@gmail.com>
2023-07-04 16:44:28 +0200

    test

--- srcs/main.c
+++ srcs/main.c
@@ -233,7 +233,65 @@ void sort_5_elements(Stack *stack_a, Stack *stack_b) {
     pa(stack_a, stack_b);
     pa(stack_a, stack_b);
 }
+static int get_max_bits(Stack *stack)
+{
+    Node *head = stack->head;
+    int max = 0;
+    int max_bits = 0;
 
+    while (head)
+    {
+        if (head->data > max)
+            max = head->data;
+        head = head->next;
+    }
+
+    while ((max >> max_bits) != 0)
+        max_bits++;
+
+    return max_bits;
+}
+
+void radix_sort(Stack *stack_a, Stack *stack_b)
+{
+    Node *head_a = stack_a->head;
+    int i = 0;
+    int j = 0;
+    int size = 0;
+    int max_bits = 0;
+
+    while (head_a)
+    {
+        size++;
+        head_a = head_a->next;
+    }
+
+    max_bits = get_max_bits(stack_a);
+
+    i = 0;
+    while (i < max_bits)
+    {
+        j = 0;
+        head_a = stack_a->head;
+
+        while (j < size)
+        {
+            head_a = stack_a->head;
+
+            if (((head_a->data >> i) & 1) == 1)
+                ra(stack_a);
+            else
+                pb(stack_a, stack_b);
+
+            j++;
+        }
+
+        while (stack_b->head != NULL)
+            pa(stack_a, stack_b);
+
+        i++;
+    }
+}
 int main(int argc, char **argv) {
     Stack *stack_a;
     Stack *stack_b;
@@ -250,11 +308,12 @@ int main(int argc, char **argv) {
     if (issorted(stack_a) == true) {
         exit(EXIT_SUCCESS);
     }
-//    print_stack(stack_a);
     if (argc == 4)
         sort_3_elements(stack_a);
     if (argc == 6)
         sort_5_elements(stack_a, stack_b);
- //print_stack(stack_a);
+    else if (argc > 100) {
+        radix_sort(stack_a, stack_b);
+    }
      return (0);
 }
